43ff86440dcba6588711e14a893d2c93f4eb7daa
hdst@local.husbanken.no <sturla.solheim@husbanken.no>
2025-11-12 14:18:19 +0100

    lagre

--- nvim/.config/nvim/lua/plugins/java.lua
+++ nvim/.config/nvim/lua/plugins/java.lua
@@ -1,45 +1,43 @@
 return {
   "nvim-java/nvim-java",
-  config = false,
   dependencies = {
-    {
-      "neovim/nvim-lspconfig",
-      opts = {
-        servers = {
-          -- Your JDTLS configuration goes here
-          jdtls = {
-            -- settings = {
-            --   java = {
-            --     configuration = {
-            --       runtimes = {
-            --         {
-            --           name = "JavaSE-23",
-            --           path = "/usr/local/sdkman/candidates/java/23-tem",
-            --         },
-            --       },
-            --     },
-            --   },
-            -- },
+    "nvim-java/lua-async-await",
+    "nvim-java/nvim-java-core",
+    "nvim-java/nvim-java-test",
+    "nvim-java/nvim-java-dap",
+    "MunifTanjim/nui.nvim",
+    "neovim/nvim-lspconfig",
+    "mfussenegger/nvim-dap",
+  },
+  config = function()
+    -- Setup nvim-java before lspconfig
+    require("java").setup({
+      root_markers = {
+        ".git",
+        "pom.xml",
+      },
+    })
+
+    -- Get capabilities from nvim-cmp if available
+    local has_cmp, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
+    local capabilities = has_cmp and cmp_nvim_lsp.default_capabilities() or vim.lsp.protocol.make_client_capabilities()
+
+    -- Configure jdtls with vim.lsp.config
+    vim.lsp.config('jdtls', {
+      capabilities = capabilities,
+      settings = {
+        java = {
+          configuration = {
+            runtimes = {
+              -- Uncomment and adjust if you have multiple Java versions
+              -- {
+              --   name = "JavaSE-23",
+              --   path = "/usr/local/sdkman/candidates/java/23-tem",
+              -- },
+            },
           },
         },
-        setup = {
-          jdtls = function()
-            -- Your nvim-java configuration goes here
-            require("java").setup({
-              -- root_markers = {
-              --   "settings.gradle",
-              --   "settings.gradle.kts",
-              --   "pom.xml",
-              --   "build.gradle",
-              --   "mvnw",
-              --   "gradlew",
-              --   "build.gradle",
-              --   "build.gradle.kts",
-              -- },
-            })
-          end,
-        },
       },
-    },
-  },
+    })
+  end,
 }
